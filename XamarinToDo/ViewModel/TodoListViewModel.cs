0f893dcc58da6620d3dfd3f3cc0d27e5fa9d8ae1
Jael Ngutu <jaelngutu@gmail.com>
2022-04-22 00:58:17 +0300

    Add project files.

---
+++ XamarinToDo/ViewModel/TodoListViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using XamarinToDo.Model;
+
+namespace XamarinToDo.ViewModel
+{
+    public class TodoListViewModel
+    {
+        //view can observe can update
+        public ObservableCollection<TodoItem> TodoItems { get; set; }
+
+        public TodoListViewModel()
+        {
+            TodoItems = new ObservableCollection<TodoItem>();
+
+            TodoItems.Add(new TodoItem("First Todo", true));
+            TodoItems.Add(new TodoItem("First Todo", true));
+            TodoItems.Add(new TodoItem("First Todo", true));
+        }
+    }
+}
